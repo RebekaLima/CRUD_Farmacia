0e717ae0dce4db2976a89c9438798af55512e3c3
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-01-27 09:42:13 -0300

    Commit inicial

---
+++ src/main/java/com/farma/farmalife/FarmalifeApplication.java
@@ -0,0 +1,13 @@
+package com.farma.farmalife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmalifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmalifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farma/farmalife/FarmalifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farma.farmalife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmalifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
